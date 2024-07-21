1eb873327fb6021337e4747f20c22a6ef63f95d6
Furkan Gürsoy <furkngrsy@icloud.com>
2024-07-21 16:03:15 +0300

    added controller for domain models

--- Para.Api/Para.Bussiness/Validations/CustomerValidator.cs
+++ Para.Api/Para.Bussiness/Validations/CustomerValidator.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentValidation;
+using Para.Schema;
 
-namespace Para.Bussiness.Validation
+namespace Para.Bussiness.Validations
 {
-    internal class CustomerValidation
+    public class CustomerValidator : AbstractValidator<CustomerRequest>
     {
+        public CustomerValidator()
+        {
+            RuleFor(x => x.FirstName)
+                .NotEmpty().WithMessage("First Name is required.")
+                .Length(2, 50).WithMessage("First Name must be between 2 and 50 characters.");
+
+            RuleFor(x => x.LastName)
+                .NotEmpty().WithMessage("Last Name is required.")
+                .Length(2, 50).WithMessage("Last Name must be between 2 and 50 characters.");
+
+            RuleFor(x => x.IdentityNumber)
+                .NotEmpty().WithMessage("Identity Number is required.")
+                .Length(11).WithMessage("Identity Number must be 11 characters long.");
+
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email is required.")
+                .EmailAddress().WithMessage("Invalid email format.");
+
+            RuleFor(x => x.DateOfBirth)
+                .LessThan(DateTime.Now).WithMessage("Date of Birth must be in the past.");
+        }
     }
 }
